69cbb8de88d06b668c48909dfd85df5fd243fcea
lalithkumaralladi <2410030508@klh.edu.in>
2025-11-04 19:19:58 +0530

    Delete backend directory

--- backend/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Fraud Detection Models
-
-This package contains the machine learning models for fraud detection.
-"""
-
-from .fraud_detector import FraudDetector
-
-__all__ = ['FraudDetector']
